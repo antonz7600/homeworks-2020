65846f188ae37d71d03363e159514d5262aba169
antonz7600 <antonz7600@yandex.ru>
2020-04-04 20:51:38 +0300

    feat: add all functionality and tests

---
+++ HW04/Anton_Zavaliuk/lib/custom_array.rb
@@ -0,0 +1,25 @@
+module CustomArray
+  def my_each
+    i = 0
+    while i < length
+      yield self[i]
+      i += 1
+    end
+  end
+
+  def my_map(&block)
+    result = []
+    my_each do |element|
+      result << block.call(element)
+    end
+    result
+  end
+
+  def my_select(&block)
+    result = []
+    my_each do |element|
+      result << element if block.call(element) == true
+    end
+    result
+  end
+end
--- HW04/Anton_Zavaliuk/task1.rb
+++ HW04/Anton_Zavaliuk/task1.rb
@@ -1,1 +1,8 @@
-p "Hello, branch"
+require_relative 'lib/array'
+
+# it is JUST small test, all rspec tests are inside test folder
+
+test = Array.new([42, 52, 52, 'aff'])
+test = test.my_select { |x| x.class == Integer }
+p test
+p test.custom?
