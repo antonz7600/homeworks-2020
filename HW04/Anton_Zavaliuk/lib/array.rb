f9570527d7e5d655fce26e244e9e8765b9736062
antonz7600 <antonz7600@yandex.ru>
2020-04-16 23:32:45 +0300

    style: remove git symbols from files

--- HW04/Anton_Zavaliuk/lib/array.rb
+++ HW04/Anton_Zavaliuk/lib/array.rb
@@ -1,13 +1,9 @@
-<<<<<<< HEAD
 require_relative 'custom_array'
-
-=======
 # frozen_string_literal: true
 
 require_relative 'custom_array'
 
-# Custom array add
->>>>>>> f3e4b569a5afcc0912545238b2e30b7db7661186
+# Custom array merge with Array class
 class Array
   include CustomArray
   def custom?
--- HW04/Anton_Zavaliuk/task1.rb
+++ HW04/Anton_Zavaliuk/task1.rb
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 # frozen_string_literal: true
 
->>>>>>> f3e4b569a5afcc0912545238b2e30b7db7661186
 require_relative 'lib/array'
 
 # it is JUST small test, all rspec tests are inside test folder
@@ -12,9 +8,3 @@ test = Array.new([42, 52, 52, 'aff'])
 test = test.my_select { |x| x.class == Integer }
 p test
 p test.custom?
-<<<<<<< HEAD
-=======
-
->>>>>>> init: add task file
-=======
->>>>>>> f3e4b569a5afcc0912545238b2e30b7db7661186
